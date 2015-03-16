c06d741362203415143ae036768b6a7a8153a321
Marc Gayle <marc.gayle@gmail.com>
2015-03-16 08:45:59 -0500

    Blog is basically ready to go, before gh-pages extension is added.

--- config.rb
+++ config.rb
@@ -185,18 +185,16 @@ helpers do
 end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
