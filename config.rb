75d3f247a475fce55806e418e947cc4699eee66f
Marc Gayle <marc.gayle@gmail.com>
2015-03-16 01:57:24 -0500

    Initial config for blog settings.

--- config.rb
+++ config.rb
@@ -2,19 +2,19 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
+Time.zone = "Lima"
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
+  blog.taglink = "tags/{tag}.html"
+  blog.layout = "layout"
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 250
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
@@ -24,9 +24,9 @@ activate :blog do |blog|
   blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 20
+  blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
@@ -101,3 +101,6 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# Pretty URLs
+activate :directory_indexes
